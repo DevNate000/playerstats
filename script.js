1e12de9ff32affedb6bfe243f1ded2adbae7df7f
DevNate000 <124859950+DevNate000@users.noreply.github.com>
2023-03-30 10:09:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-fetch(`https://scraper.nt-verse.com/api/racer/nathaniel818)`).then((data)=>{
+fetch(`https://scraper.nt-verse.com/api/racer/nathaniel818`).then((data)=>{
     // console.log(data);
         return data.json();
 }).then((objectData)=>{
     let tableData=`<tr>
-    objectData.map((values)=>{
-        tableData+=`<tr>
-        <td>${values.displayName}</td>
-        <td>${values.username}</td>
-        <td>${values.racesPlayed}</td>
-        <td>${values.avgSpeed}</td>
-        <td>${values.level}</td>
-        <td><img src="${values.image}"/></td>
+        <td>${objectData.displayName}</td>
+        <td>${objectData.username}</td>
+        <td>${objectData.racesPlayed}</td>
+        <td>${objectData.avgSpeed}</td>
+        <td>${objectData.level}</td>
+        <td><img src="${objectData.image}"/></td>
         </tr>`;
-    });
     document.getElementById("table_body").innerHTML=tableData;
 })  .catch((err)=>{
     console.log(err);
