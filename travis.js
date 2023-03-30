2b14ec074271ee5680916cdbbcb1f0090768b99f
DevNate000 <124859950+DevNate000@users.noreply.github.com>
2023-03-30 14:47:53 -0400

    Add files via upload

--- travis.js
+++ travis.js
@@ -3,9 +3,8 @@ fetch(`https://scraper.nt-verse.com/api/racer/travis`).then((data)=>{
         return data.json();
 }).then((objectData)=>{
     let tableData=`<tr>
-        <td>${objectData.tag}</td>
-        <td>${objectData.displayName}</td>
-        <td>${objectData.username}</td>
+    <td>[<a href="https://www.nitrotype.com/team/" + ${objectData.tag}>${objectData.tag}</a>]</td>
+    <td><a href="https://www.nitrotype.com/" + ${objectData.username}>${objectData.displayName}</a></td>
         <td>${objectData.racesPlayed}</td>
         <td>${objectData.avgSpeed}</td>
         <td>${objectData.highestSpeed}</td>
