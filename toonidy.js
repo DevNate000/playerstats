f8631e51699d99a26d63e9b12667c8a3359dff74
DevNate000 <124859950+DevNate000@users.noreply.github.com>
2023-03-30 16:03:54 -0400

    Create toonidy.js

---
+++ toonidy.js
@@ -0,0 +1,19 @@
+fetch(`https://scraper.nt-verse.com/api/racer/toonidy`).then((data)=>{
+    // console.log(data);
+        return data.json();
+}).then((objectData)=>{
+    let tableData=`<tr>
+    <td>[<a href="https://www.nitrotype.com/team/" + ${objectData.tag}>${objectData.tag}</a>]</td>
+    <td><a href="https://www.nitrotype.com/" + ${objectData.username}>${objectData.displayName}</a></td>
+        <td>${objectData.racesPlayed}</td>
+        <td>${objectData.avgSpeed}</td>
+        <td>${objectData.highestSpeed}</td>
+        <td>${objectData.level}</td>
+        <td>${objectData.longestSession}</td>
+        <td>${objectData.profileViews}</td>
+        <td>${objectData.totalCars}</td>
+        </tr>`;
+    document.getElementById("table_body").innerHTML=tableData;
+})  .catch((err)=>{
+    console.log(err);
+})
