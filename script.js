510b9683a1488d5471e4de32243267c91a0099f9
DevNate000 <124859950+DevNate000@users.noreply.github.com>
2023-03-30 13:15:49 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -3,14 +3,16 @@ fetch(`https://scraper.nt-verse.com/api/racer/nathaniel818`).then((data)=>{
         return data.json();
 }).then((objectData)=>{
     let tableData=`<tr>
+        <td>${objectData.tag}</td>
         <td>${objectData.displayName}</td>
         <td>${objectData.username}</td>
         <td>${objectData.racesPlayed}</td>
         <td>${objectData.avgSpeed}</td>
+        <td>${objectData.highestSpeed}</td>
         <td>${objectData.level}</td>
-        <td>${objectData.level}</td>
-        <td>${objectData.level}</td>
-        <td>${objectData.level}</td>
+        <td>${objectData.longestSession}</td>
+        <td>${objectData.profileViews}</td>
+        <td>${objectData.totalCars}</td>
         </tr>`;
     document.getElementById("table_body").innerHTML=tableData;
 })  .catch((err)=>{
