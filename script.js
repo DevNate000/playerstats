78248e87450f67c48595427faae8bf6c96f32a9f
DevNate000 <124859950+DevNate000@users.noreply.github.com>
2023-03-30 22:56:04 -0400

    Add files via upload
    update :D

--- script.js
+++ script.js
@@ -11,9 +11,10 @@ fetch(`https://scraper.nt-verse.com/api/racer/nathaniel818`).then((data)=>{
         return data.json();
 }).then((objectData)=>{
     let tableData=`<tr>
-    <td><img class="tinyimg" src="car_nate.png" alt="ytdriver"></td>                  
-    <td>[<a href="https://www.nitrotype.com/team/" + ${objectData.tag}>${objectData.tag}</a>]</td>
-    <td><a href="https://www.nitrotype.com/" + ${objectData.username}>${objectData.displayName}</a></td>
+    <td><img class="tinyimg" src="car_nate.png" alt="ytdriver"></td>  
+    <td><img class="tinyimg" src="https://www.nitrotype.com/cars/painted/${objectData.carID}_large_1_10.png" alt="carimg"></td>                
+    <td>[<a href="https://www.nitrotype.com/team/${objectData.tag}">${objectData.tag}</a>]</td>
+    <td><a href="https://www.nitrotype.com/racer/${objectData.username}">${objectData.username}</a></td>
         <td class="races">${objectData.racesPlayed}</td>
         <td>${objectData.avgSpeed}</td>
         <td>${objectData.highestSpeed}</td>
@@ -21,6 +22,7 @@ fetch(`https://scraper.nt-verse.com/api/racer/nathaniel818`).then((data)=>{
         <td>${objectData.longestSession}</td>
         <td class="views">${objectData.profileViews}</td>
         <td>${objectData.totalCars}</td>
+        <td>${objectData.createdStamp /86400 + 3/30/23 * (1970,1,1)-0.167}</td>
         </tr>`;
     document.getElementById("nathaniel818_table").innerHTML=tableData;
 })  .catch((err)=>{
@@ -35,9 +37,10 @@ fetch(`https://scraper.nt-verse.com/api/racer/toonidy`).then((data)=>{
         return data.json();
 }).then((objectData)=>{
     let tableData=`<tr>
-    <td><img class="tinyimg" src="car_toonidy.png" alt="ytdriver"></td>                  
-    <td>[<a href="https://www.nitrotype.com/team/" + ${objectData.tag}>${objectData.tag}</a>]</td>
-    <td><a href="https://www.nitrotype.com/" + ${objectData.username}>${objectData.displayName}</a></td>
+    <td><img class="tinyimg" src="car_toonidy.png" alt="ytdriver"></td> 
+    <td><img class="tinyimg" src="https://www.nitrotype.com/cars/painted/${objectData.carID}_large_1_10.png" alt="carimg"></td>                 
+    <td>[<a href="https://www.nitrotype.com/team/${objectData.tag}">${objectData.tag}</a>]</td>
+    <td><a href="https://www.nitrotype.com/racer/${objectData.username}">${objectData.username}</a></td>
         <td>${objectData.racesPlayed}</td>
         <td>${objectData.avgSpeed}</td>
         <td>${objectData.highestSpeed}</td>
@@ -45,6 +48,7 @@ fetch(`https://scraper.nt-verse.com/api/racer/toonidy`).then((data)=>{
         <td>${objectData.longestSession}</td>
         <td>${objectData.profileViews}</td>
         <td>${objectData.totalCars}</td>
+        <td>${objectData.createdStamp /86400 + 3/30/23 * (1970,1,1)-0.167}</td>
         </tr>`;
     document.getElementById("toonidy_table").innerHTML=tableData;
 })  .catch((err)=>{
@@ -59,9 +63,10 @@ fetch(`https://scraper.nt-verse.com/api/racer/travis`).then((data)=>{
         return data.json();
 }).then((objectData)=>{
     let tableData=`<tr>
-    <td><img class="tinyimg" src="car_travis.png" alt="ytdriver"></td>                  
-    <td>[<a href="https://www.nitrotype.com/team/" + ${objectData.tag}>${objectData.tag}</a>]</td>
-    <td><a href="https://www.nitrotype.com/" + ${objectData.username}>${objectData.displayName}</a></td>
+    <td><img class="tinyimg" src="car_travis.png" alt="ytdriver"></td>    
+    <td><img class="tinyimg" src="https://www.nitrotype.com/cars/painted/${objectData.carID}_large_1_10.png" alt="carimg"></td>
+    <td>[<a href="https://www.nitrotype.com/team/${objectData.tag}">${objectData.tag}</a>]</td>
+    <td><a href="https://www.nitrotype.com/racer/${objectData.username}">${objectData.username}</a></td>
         <td>${objectData.racesPlayed}</td>
         <td>${objectData.avgSpeed}</td>
         <td>${objectData.highestSpeed}</td>
@@ -69,6 +74,7 @@ fetch(`https://scraper.nt-verse.com/api/racer/travis`).then((data)=>{
         <td>${objectData.longestSession}</td>
         <td>${objectData.profileViews}</td>
         <td>${objectData.totalCars}</td>
+        <td>${objectData.createdStamp /86400 + 3/30/23 * (1970,1,1)-0.167}</td>
         </tr>`;
     document.getElementById("travis_table").innerHTML=tableData;
 })  .catch((err)=>{
